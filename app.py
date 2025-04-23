2e9e54be4d701cf6de903be5655ea39afd5d071f
Rollz88 <rollz138@googlemail.com>
2025-04-23 16:05:50 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="RVL-Bot", layout="centered")
+st.title("Willkommen beim RVL-Bot 👋")
+st.write("Dein persönlicher KI-Assistent für den Arbeitsalltag ist startklar!")
